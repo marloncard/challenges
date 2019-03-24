62bd543398191b5547a6a421c3228a010ef2006d
Marlon Card <marloncard@gmail.com>
2019-03-24 16:50:52 -0400

    Complete challenge 02

--- 02/game.py
+++ 02/game.py
@@ -4,6 +4,7 @@
 
 import itertools
 import random
+import string
 
 from data import DICTIONARY, LETTER_SCORES, POUCH
 
@@ -12,19 +13,30 @@ NUM_LETTERS = 7
 
 def draw_letters():
     """Pick NUM_LETTERS letters randomly. Hint: use stdlib random"""
-    pass
+    return "".join([random.choice(string.ascii_uppercase) for n in range(NUM_LETTERS)])
 
 
 def input_word(draw):
     """Ask player for a word and validate against draw.
     Use _validation(word, draw) helper."""
-    pass 
-
+    while True:
+        word = input("Enter word: ")
+        try:
+            _validation(word, draw)
+            return word
+        except ValueError:
+            continue
 
 
 def _validation(word, draw):
     """Validations: 1) only use letters of draw, 2) valid dictionary word"""
-    pass
+    for l in word:
+        if l.upper() not in draw:
+            print("Invalid letter: " + l)
+            raise ValueError
+    if word not in DICTIONARY:
+        print("Invalid word: " + word)
+        raise ValueError
 
 
 # From challenge 01:
@@ -40,13 +52,24 @@ def calc_word_value(word):
 def get_possible_dict_words(draw):
     """Get all possible words from draw which are valid dictionary words.
     Use the _get_permutations_draw helper and DICTIONARY constant"""
-    pass
+    """
+    possible_words = []
+    for word in _get_permutations_draw(draw):
+        if word in DICTIONARY:
+            possible_words.append(word)
+    """
+    s1 = set("".join(i).lower() for i in _get_permutations_draw(draw))
+    s2 = set(DICTIONARY)
+    return list(s1.intersection(s2))
 
 
 def _get_permutations_draw(draw):
     """Helper for get_possible_dict_words to get all permutations of draw letters.
     Hint: use itertools.permutations"""
-    pass
+    permu_list = []
+    for n in range(1, NUM_LETTERS+1):
+        permu_list.extend(itertools.permutations(draw, n))
+    return permu_list
 
 
 # From challenge 01:
